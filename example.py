d08e6207611b3e9c0afbcd0133a59ac52372cf0c
hannxu <48928341+hannxu123@users.noreply.github.com>
2023-07-05 13:39:46 -0400

    Add files via upload

--- example.py
+++ example.py
@@ -7,14 +7,10 @@ def write_to_jsonl(file_path, data):
         f.write('\n')
 
 # Example data
-data1 = {'name': 'John', 'age': 30, 'city': 'New York'}
-data2 = {'name': 'Jane', 'age': 25, 'city': 'San Francisco'}
-data3 = {'name': 'Bob', 'age': 40, 'city': 'Chicago'}
-
-# File path
 file_path = 'data.jsonl'
 
-# Write data to the JSONL file
-write_to_jsonl(file_path, data1)
-write_to_jsonl(file_path, data2)
-write_to_jsonl(file_path, data3)
+for j in range(5):
+    data1 = {'model': 'gpt-3.5-turbo', 'max_tokens': 30, 'messages': [{'role': 'user', 'content': 'What is 1 + ' + str(j)}]}
+
+    # Write data to the JSONL file
+    write_to_jsonl(file_path, data1)
